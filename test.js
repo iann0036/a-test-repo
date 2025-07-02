6cb17db3144a4d72fc511957a6af9fe3d63dc641
Ian Mckay <contact@ianmckay.com.au>
2025-07-02 23:39:27 +1000

    A demo

---
+++ test.js
@@ -0,0 +1,26 @@
+
+function this_is_a_test() {
+  // This is a test function
+  console.log("This is a test function");
+}
+
+function another_test_function() {
+  // This is another test function
+  console.log("This is another test function");
+}
+
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+function makelongerid() {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+}
