785f72688710b4d2820bdef81a0b7e8a9c22c34c
Ian Mckay <contact@ianmckay.com.au>
2025-07-03 21:40:22 +1000

    Some changes

---
+++ test.js
@@ -0,0 +1,24 @@
+function my_function() {
+  return "Hi";
+}
+
+function my_other_function() {
+  return "Hello";
+}
+
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+export function foo() {}
+
+export function bar() {
+  return "bar";
+}
+
