fe12b7be81b9109d8b29b246b611611de0468c8c
Ian Mckay <contact@ianmckay.com.au>
2025-07-04 17:20:23 +1000

    Files

---
+++ test.js
@@ -0,0 +1,20 @@
+// this is my code
+
+// this is something else
+
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+export function foo() {}
+
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
