5a35d3a028b681a008defead8e5450a0b974c251
Ian Mckay <contact@ianmckay.com.au>
2025-07-04 12:34:48 +1000

    Some files

---
+++ test1.js
@@ -0,0 +1,20 @@
+// This is a new file
+
+// Some comment
+
+function test() {
+  console.log("This is a test function");
+}
+
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+export function foo() {}
+
